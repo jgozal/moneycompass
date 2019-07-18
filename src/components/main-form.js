7ce94df214acfef75b5b2049eb32ad1e43d2c1a6
David Zhu <hdavidzhu@gmail.com>
2019-07-18 10:56:28 -0700

    Auto format main-form.js

--- src/components/main-form.js
+++ src/components/main-form.js
@@ -3,16 +3,15 @@
 // https://www.nerdwallet.com/mortgages/mortgage-rates/30-year-fixed
 // https://michaelbluejay.com/house/15vs30.html
 
-import _ from 'lodash';
-import React from 'react';
-import numeral from 'numeral';
-import styled, { css } from "react-emotion";
-import { Input } from 'reactstrap';
-import { FV, PMT } from 'formulajs/lib/financial';
+import _ from 'lodash'
+import React from 'react'
+import styled from 'react-emotion'
+import { Input } from 'reactstrap'
+import { FV, PMT } from 'formulajs/lib/financial'
 
 // DEFAULT VALUES
 
-const COMPOUND_FREQUENCY = 12;
+const COMPOUND_FREQUENCY = 12
 
 const DEFAULT_OPTION = {
   interestAmt: 0,
@@ -20,16 +19,16 @@ const DEFAULT_OPTION = {
   term: 0,
   pmt: 0,
   fv: 0
-};
+}
 
-let option1 = _.cloneDeep(DEFAULT_OPTION);
-let option2 = _.cloneDeep(DEFAULT_OPTION);
+let option1 = _.cloneDeep(DEFAULT_OPTION)
+let option2 = _.cloneDeep(DEFAULT_OPTION)
 
-option1.interestRate = 4.3;
-option1.term = 30;
+option1.interestRate = 4.3
+option1.term = 30
 
-option2.interestRate = 4;
-option2.term = 15;
+option2.interestRate = 4
+option2.term = 15
 
 // CSS
 
@@ -39,167 +38,181 @@ const Row = styled('div')`
 `
 
 class MainForm extends React.Component {
-
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.calculatePMT = this.calculatePMT.bind(this)
     this.calculateInterestAmt = this.calculateInterestAmt.bind(this)
     this.calculateFV = this.calculateFV.bind(this)
-    this.calculateOpportunityCost = this.calculateOpportunityCost.bind(this);
+    this.calculateOpportunityCost = this.calculateOpportunityCost.bind(this)
     this.updateInput = this.updateInput.bind(this)
-    this.calculateAll = this.calculateAll.bind(this);
+    this.calculateAll = this.calculateAll.bind(this)
     this.state = {
       investmentRate: 9,
       loanAmt: 200000,
       inflation: 2,
       optCost: 0,
       options: {
-        option1, option2
+        option1,
+        option2
       }
     }
   }
 
-  componentWillMount() {
+  componentWillMount () {
     // Running all calculations with default values on page load
-    this.setState(this.calculateAll(this.state));
+    this.setState(this.calculateAll(this.state))
   }
 
   // Returns monthly payment.
-  calculatePMT(o, state) {
-    return PMT((o.interestRate - state.inflation) / 100 / COMPOUND_FREQUENCY, o.term * COMPOUND_FREQUENCY, state.loanAmt);
+  calculatePMT (o, state) {
+    return PMT(
+      (o.interestRate - state.inflation) / 100 / COMPOUND_FREQUENCY,
+      o.term * COMPOUND_FREQUENCY,
+      state.loanAmt
+    )
   }
 
   // Returns interest amount and depends on payment (PMT).
-  calculateInterestAmt(o, state) {
-    return o.pmt * (o.term * COMPOUND_FREQUENCY) + state.loanAmt;
+  calculateInterestAmt (o, state) {
+    return o.pmt * (o.term * COMPOUND_FREQUENCY) + state.loanAmt
   }
 
   // Returns future value dynamically depending on mortgage term length.
-  calculateFV(option1, option2, state) {
+  calculateFV (option1, option2, state) {
     // TODO: what if terms of option1 and option2 are equal?
     if (option1.term > option2.term) {
-      return FV((state.investmentRate - state.inflation) / 100 / COMPOUND_FREQUENCY, option1.term * COMPOUND_FREQUENCY, option2.pmt - option1.pmt, 0);
+      return FV(
+        (state.investmentRate - state.inflation) / 100 / COMPOUND_FREQUENCY,
+        option1.term * COMPOUND_FREQUENCY,
+        option2.pmt - option1.pmt,
+        0
+      )
     } else if (option1.term < option2.term) {
-      return FV((state.investmentRate - state.inflation) / 100 / COMPOUND_FREQUENCY, (option2.term - option1.term) * COMPOUND_FREQUENCY, option1.pmt, 0);
+      return FV(
+        (state.investmentRate - state.inflation) / 100 / COMPOUND_FREQUENCY,
+        (option2.term - option1.term) * COMPOUND_FREQUENCY,
+        option1.pmt,
+        0
+      )
     }
   }
 
   // Returns opportunity cost of choosing option1 over option2. Opportunity cost can be positive and negative.
-  calculateOpportunityCost(option1, option2) {
-    return (option1.fv + option1.interestAmt) - (option2.fv + option2.interestAmt);
+  calculateOpportunityCost (option1, option2) {
+    return option1.fv + option1.interestAmt - (option2.fv + option2.interestAmt)
   }
 
   // Runs every time an input is updated and uses input's name tag to make specific changes in the state
-  updateInput(e) {
-    let state = Object.assign({}, this.state);
-    const key = e.target.name;
+  updateInput (e) {
+    let state = Object.assign({}, this.state)
+    const key = e.target.name
 
-    const value = parseFloat(e.target.value);
+    const value = parseFloat(e.target.value)
 
-    _.set(state, key, value);
+    _.set(state, key, value)
     if (!isNaN(value)) {
-      state = this.calculateAll(state);
+      state = this.calculateAll(state)
     }
-    this.setState(state);
+    this.setState(state)
   }
 
   // Runs all calculations and returns a modified state
-  calculateAll(state) {
-    option1 = state.options.option1;
-    option2 = state.options.option2;
+  calculateAll (state) {
+    option1 = state.options.option1
+    option2 = state.options.option2
 
-    option1.pmt = this.calculatePMT(option1, state);
-    option2.pmt = this.calculatePMT(option2, state);
+    option1.pmt = this.calculatePMT(option1, state)
+    option2.pmt = this.calculatePMT(option2, state)
 
-    option1.interestAmt = this.calculateInterestAmt(option1, state);
-    option2.interestAmt = this.calculateInterestAmt(option2, state);
+    option1.interestAmt = this.calculateInterestAmt(option1, state)
+    option2.interestAmt = this.calculateInterestAmt(option2, state)
 
-    option1.fv = this.calculateFV(option1, option2, state);
-    option2.fv = this.calculateFV(option2, option1, state);
+    option1.fv = this.calculateFV(option1, option2, state)
+    option2.fv = this.calculateFV(option2, option1, state)
 
-    state.optCost = this.calculateOpportunityCost(option1, option2);
+    state.optCost = this.calculateOpportunityCost(option1, option2)
 
-    return state;
+    return state
   }
 
-  render() {
+  render () {
     return (
-      <div className="main-container">
-        <div className="section">
+      <div className='main-container'>
+        <div className='section'>
           <h5>Loan Amount</h5>
           <Input
-            name="loanAmt"
-            placeholder="Loan Amount"
-            type="number"
+            name='loanAmt'
+            placeholder='Loan Amount'
+            type='number'
             value={this.state.loanAmt}
             onChange={this.updateInput}
           />
         </div>
-        <div className="section">
+        <div className='section'>
           <h5>Loan Term</h5>
           <Row>
             <Input
-              name="options.option1.term"
-              placeholder="Loan Term"
-              type="number"
+              name='options.option1.term'
+              placeholder='Loan Term'
+              type='number'
               value={option1.term}
               onChange={this.updateInput}
             />
             <Input
-              name="options.option2.term"
-              placeholder="Loan Term"
-              type="number"
+              name='options.option2.term'
+              placeholder='Loan Term'
+              type='number'
               value={option2.term}
               onChange={this.updateInput}
             />
           </Row>
         </div>
-        <div className="section">
+        <div className='section'>
           <h5>Interest/APR</h5>
           <Row>
             <Input
-              name="options.option1.interestRate"
-              placeholder="APR"
-              type="number"
+              name='options.option1.interestRate'
+              placeholder='APR'
+              type='number'
               value={option1.interestRate}
               onChange={this.updateInput}
             />
             <Input
-              name="options.option2.interestRate"
-              placeholder="APR"
-              type="number"
+              name='options.option2.interestRate'
+              placeholder='APR'
+              type='number'
               value={option2.interestRate}
               onChange={this.updateInput}
             />
           </Row>
         </div>
-        <div className="section">
+        <div className='section'>
           <h5>Return on Investment (ROI)</h5>
           <Input
-            name="investmentRate"
-            placeholder="ROI"
-            type="number"
+            name='investmentRate'
+            placeholder='ROI'
+            type='number'
             value={this.state.investmentRate}
             onChange={this.updateInput}
           />
         </div>
-        <div className="section">
+        <div className='section'>
           <h5>Inflation</h5>
           <Input
-            name="inflation"
-            placeholder="Inflation"
-            type="number"
+            name='inflation'
+            placeholder='Inflation'
+            type='number'
             value={this.state.inflation}
             onChange={this.updateInput}
           />
         </div>
 
-        <div className="result">
-          <pre>{JSON.stringify(this.state, null, "\t")}</pre>
+        <div className='result'>
+          <pre>{JSON.stringify(this.state, null, '\t')}</pre>
         </div>
       </div>
     )
   }
 }
 
-export default MainForm;
+export default MainForm
