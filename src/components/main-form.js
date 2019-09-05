ab7e8e509cb4a2bdbddefb99a7b246eda4725007
David Zhu <hdavidzhu@gmail.com>
2019-09-05 08:50:04 -0700

    Port over all the copy

--- src/components/main-form.js
+++ src/components/main-form.js
@@ -282,6 +282,19 @@ class MainForm extends React.Component {
               has an excellent article which outlines the pros and cons of
               15-year versus 30-year mortgages.
             </Accordion>
+            <Accordion
+              title={`What happens when after you’re done paying off the ${_.min(
+                [option1.term, option2.term]
+              )}-year mortgage?`}
+            >
+              {/* TODO: Convert to dynamic numbers */}
+              This tool assumes that you’ll invest the difference between the
+              payment of the 15-year and the 30-year mortgages. If you had to
+              make monthly payments of $1,479.38 for your 15-year mortgage
+              versus $989.74 for your 30 year mortgage, you would invest the
+              difference ($489.64) monthly after paying off your 15-year
+              mortgage.
+            </Accordion>
           </Section>
 
           <Section>
@@ -318,6 +331,18 @@ class MainForm extends React.Component {
                 </InputGroup>
               </InputWrapper>
             </InputContainer>
+            <Accordion title='What is the annual percentage rate (APR)?'>
+              APR measures how much it costs to borrow money.{' '}
+              <a href='https://www.bankrate.com/glossary/a/apr/'>Bankrate</a>{' '}
+              provides a more in-depth explanation.
+            </Accordion>
+            <Accordion title='Fixed vs Variable Interest Rates'>
+              This tool assumes your mortgage interest rate is fixed.{' '}
+              <a href='https://www.valuepenguin.com/loans/fixed-vs-variable-interest-rates'>
+                Value Penguin
+              </a>{' '}
+              has a great article about how fixed and variable rates work.
+            </Accordion>
           </Section>
 
           <Section>
@@ -334,6 +359,62 @@ class MainForm extends React.Component {
                 <InputGroupText>%</InputGroupText>
               </InputGroupAddon>
             </InputGroup>
+            <Accordion title='How do I invest?'>
+              {/* TODO: Could this be Vanguard instead? */}
+              Investing refers to the process of allocating money to something
+              that you expect to generate income from or/and that you hope will
+              appreciate (increase in value). You can invest in anything: real
+              estate, index funds, stocks, bonds etc. Most times, you will need
+              a brokerage account to invest.{' '}
+              <a href='https://www.fidelity.com/'>Fidelity</a> provides a great
+              platform for this, with low fees, as well as great customer
+              service to help you navigate through the entire process.
+            </Accordion>
+            <Accordion title='Return on Investment (ROI)'>
+              Return on investment (ROI) is the gain or loss generated on your
+              investment relative to the amount of money invested (expressed as
+              a percentage). This tool defaults to a 9% ROI because that is
+              roughly the average historical annual return for the S&P 500.{' '}
+              <a href='https://www.investopedia.com/ask/answers/042415/what-average-annual-return-sp-500.asp'>
+                Investopedia
+              </a>{' '}
+              provides a little more background on this.
+            </Accordion>
+            <Accordion title='Risk and Diversification'>
+              Diversification is about not putting all your eggs in one basket
+              to mitigate risk (how volatile is your investment). We recommend
+              you invest in a variety of low-cost index funds to diversify as
+              best as possible.{' '}
+              <a href='https://www.thebalance.com/the-importance-of-diversification-3025567'>
+                The Balance
+              </a>{' '}
+              has a great article on diversification.
+            </Accordion>
+            <Accordion title='Long Term vs. Short Term Investments'>
+              Long term investments are those you hold for longer than a year.
+              In contrast, short term investments are usually held for a year or
+              less. Note that short term investments usually have a{' '}
+              <b>higher tax burden</b> than long term investments.{' '}
+              <a href='https://www.edwardjones.ca/financial-focus/investment-topics/short-term-vs-long-term-investments.html'>
+                Edward Jones
+              </a>
+              provides some info about the distinctions between a long term and
+              short term investment.
+            </Accordion>
+            <Accordion title='Taxable Accounts vs. Retirement Accounts'>
+              This tool assumes you invest your money through a retirement
+              account. When opening a brokerage account, you have the option to
+              invest through a taxable account, or a retirement account. Taxable
+              accounts are accounts which are subject to capital gains taxes
+              (you will have to pay taxes on your profit). Retirement accounts
+              (IRA, 401k, 403b, etc) in contrast, are tax-sheltered, but the
+              caveat is that your money suffers from some withdrawal
+              restrictions.{' '}
+              <a href='https://www.sofi.com/learn/content/what-is-a-taxable-account/'>
+                SoFi
+              </a>{' '}
+              provides a more in-depth analysis on this topic.
+            </Accordion>
           </Section>
 
           <Section>
@@ -350,6 +431,45 @@ class MainForm extends React.Component {
                 <InputGroupText>%</InputGroupText>
               </InputGroupAddon>
             </InputGroup>
+            <Accordion title=''>
+              Prices generally increase as time passes. The average home price
+              in the US during the 1960s was roughly $20,000. The average home
+              price today is roughly{' '}
+              <a href='https://www.census.gov/construction/nrs/pdf/uspricemon.pdf'>
+                $370,000
+              </a>
+              . Because prices increase, your purchasing power decreases. If you
+              had $370,000 to buy a house today and you just left the money in
+              your bank account for 10 years, it is unlikely you would be able
+              to buy the same house for that price again 10 years later. That is
+              inflation, and that is why investing is so important, to protect
+              your purchasing power against it.{' '}
+              <a href='https://www.khanacademy.org/economics-finance-domain/core-finance/inflation-tutorial/inflation-basics-tutorial/v/what-is-inflation'>
+                Khan Academy
+              </a>{' '}
+              has a great video about inflation.
+            </Accordion>
+            <Accordion title='How do you determine inflation?'>
+              The Fed sets a target inflation rate of 2%. Inflation in the US is
+              determined by the{' '}
+              <a href='https://www.bls.gov/cpi/'>Consumer Price Index (CPI)</a>.
+              Other countries have a similar way to measure inflation.{' '}
+              <a href='https://www.khanacademy.org/economics-finance-domain/core-finance/inflation-tutorial/inflation-basics-tutorial/v/inflation-data'>
+                Khan Academy
+              </a>{' '}
+              has a brief video that talks about the data behind the CPI.
+            </Accordion>
+            <Accordion title='How inflation affects your mortgage and investments'>
+              As time passes, inflation makes your mortgage payments cheaper and
+              your return on investment lower. We factor all of this
+              automatically so the final result you see on your right is not the
+              actual money you will have gained/lost in 30 years, but rather,
+              the money you will have gained/lost in 30 years adjusted to
+              today’s purchasing power. If you’re curious about what the actual
+              number would be, give inflation a value of 0%. But remember that
+              everything will be a lot more expensive in 30 years, and that is
+              why we need to include inflation.
+            </Accordion>
           </Section>
         </InputCard>
 
