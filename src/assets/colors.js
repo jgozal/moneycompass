3f15b4cc174e6eadb58c9af2e1c2a1daf95958db
David Zhu <hdvdz@icloud.com>
2019-09-18 21:10:01 -0700

    Use noto sans as the app font

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -2,6 +2,7 @@
 
 export const BLUE = '#2196F3'
 export const GRAY = '#9E9E9E'
+export const GRAY_900 = '#212121'
 export const LIGHT_BLUE = '#03A9F4'
 export const LIGHT_GRAY = '#E0E0E0'
 export const LIGHT_GREEN = '#81C784'
